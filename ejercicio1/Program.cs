65bb0e4c309e08748882e104e0c633120b8d98ba
Tifanny Artavia Corella <alejandra.artaviacorella@gmail.com>
2020-09-17 20:15:59 -0600

    solamente comenté como una prueba

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string dato;
+            //Console.WriteLine("Hola Clase");
+            //dato = Console.ReadLine();
+            //Console.WriteLine("Digito: " + dato);
+
+            //Ctrl K + Ctrl C comentar
+            //Ctrl K + Ctrl u descomentar 
+
+            int tabla;
+            int j = 0;
+
+            Console.WriteLine("Ingrese una tabla" + tabla * j);
+            tabla = int.Parse(Console.ReadLine());
+
+            while (j < 10) ;
+            {
+                Console.WriteLine("El resultado es: " + j);
+            } 
+
+        Console.ReadKey();
+    }
+}

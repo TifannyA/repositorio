e6ed1556aa30215b60cb917c0a0b1a6be6178505
Tifanny Artavia Corella <alejandra.artaviacorella@gmail.com>
2020-09-21 16:43:54 -0600

    Me puse al día con la primer clase

--- ejercicio1/Program.cs
+++ ejercicio1/Program.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ferreteria;
 
 namespace ejercicio1
 {
@@ -11,25 +12,72 @@ namespace ejercicio1
     {
         static void Main(string[] args)
         {
-            //string dato;
-            //Console.WriteLine("Hola Clase");
-            //dato = Console.ReadLine();
-            //Console.WriteLine("Digito: " + dato);
+            //    string dato;
+            //    Console.WriteLine("Hola Clase");
+            //    dato = Console.ReadLine();
+            //    Console.WriteLine("Digito: " + dato);
+            //}
 
             //Ctrl K + Ctrl C comentar
             //Ctrl K + Ctrl u descomentar 
 
-            int tabla;
-            int j = 0;
+            //    int tabla;
+            //    int j = 0;
 
-            Console.WriteLine("Ingrese una tabla" + tabla * j);
-            tabla = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Ingrese una tabla");
+            //    tabla = int.Parse(Console.ReadLine());
 
-            while (j < 10) ;
+            //    while (j<=10)
+            //    {
+            //        Console.WriteLine("El resultado es: " + tabla*j);
+            //        j++;
+            //    }
+
+            //    Console.ReadKey();
+            //}
+
+
+            //Ejemplos de switch
+            do
             {
-                Console.WriteLine("El resultado es: " + j);
-            } 
+                switch (switch_on)
+                {
+                    case: 1
+                    break;
+                    default;
 
-        Console.ReadKey();
+                }
+            } while (true);
+
+            //Ejemplos de for
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+            foreach (var item in collection)
+            {
+
+            }
+
+            //Ejemplos de arreglo
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+            int[] datos = new int[5];
+            datos[0] = 5;
+
+            for (int i = 0; i < datos.Length; i++)
+            {
+                Console.WriteLine(1);
+
+            }
+
+            
+            ferreteria.Producto clavo1 = new Producto();
+            ferreteria.Producto clavo2 = new Producto(0,"Acero",1);
+            clavo1.facturar();
+            clavo2.facturar();
+
+        }
+
     }
 }
---
+++ ejercicio1/ferreteria/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferreteria
+{
+    public class Producto
+    {
+        private int codigo;
+        protected string nombre;
+        public double precio;
+
+        public Producto() //ctor doble tap
+        {
+        }
+
+        public Producto(int pCodigo, string nom, double pPrecio) //p = parámetro
+        {
+            this.codigo = pCodigo;
+            this.nombre = nom;
+            this.precio = precio;
+
+        }
+
+        public void facturar()
+        {
+            Console.WriteLine("Paga el producto");
+        }
+    }
+}
